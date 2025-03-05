dd4b142970c4809a866aa37b08e7e81ad1e7fa17
nngtkhngoc <khanhngoc230705@gmail.com>
2025-03-05 17:35:00 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -15,7 +14,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
-app.use(helmet()); // protect app
 app.use(morgan("dev")); // log the requests
 app.use(
   cookieSession({
