c123344e54d81739d15d3a5f9175dbb88713f9e3
unknown <ungquangtri@gmail.com>
2025-06-01 16:04:00 +0700

    Update service orders controller

--- backend/validation/serviceOrdersValidation.js
+++ backend/validation/serviceOrdersValidation.js
@@ -5,22 +5,21 @@ export const createServiceOrderValidator = Joi.object({
   //   "string.empty": "Service order ID must not be empty",
   //   "any.required": "Service order ID is required",
   // }),
-  client_id: Joi.string().required().messages({
+  client_id: Joi.string().messages({
     "string.empty": "Client ID must not be empty",
     "any.required": "Client ID is required",
   }),
-  total_price: Joi.number().required().greater(0).messages({
+  total_price: Joi.number().messages({
     "any.required": "Price is required",
-    "number.greater": "Price must be greater than 0",
   }),
-  total_paid: Joi.number().required().messages({
+  total_paid: Joi.number().messages({
     "any.required": "Total paid is required",
   }),
-  status: Joi.string().required().messages({
+  status: Joi.string().messages({
     "string.empty": "Status must not be empty",
     "any.required": "Status is required",
   }),
-  total_remaining: Joi.number().required().messages({
+  total_remaining: Joi.number().messages({
     "number.empty": "Total remaining must not be empty",
     "any.required": "Total remaining is required",
   }),
