1d875f582a624c1001619f6a30b7534c2e088fe7
nngtkhngoc <khanhngoc230705@gmail.com>
2025-06-01 16:23:32 +0700

    change language in auth controller to vietnamese

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(morgan("dev")); // log the requests
 app.use(
   cookieSession({
